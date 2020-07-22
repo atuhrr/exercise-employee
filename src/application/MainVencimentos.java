9a799bcaba18f36744029f5cf9baef5cc5b29e13
Arthur Arruda <62748391+atuhrr@users.noreply.github.com>
2020-07-22 14:30:21 +0200

    First implamation MainVencimentos

---
+++ src/application/MainVencimentos.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.TrabalhadorPeca;
+
+public class MainVencimentos {
+
+    public static void main(String[] args) {
+
+        new TrabalhadorPeca("Jorge Silva", 53,62);
+
+    }
+}
