a3dfc3163859962a4bad16e3097afc063a3ef139
Arthur Arruda <62748391+atuhrr@users.noreply.github.com>
2020-07-22 14:47:10 +0200

    Matrix with size ten

--- src/application/MainVencimentos.java
+++ src/application/MainVencimentos.java
@@ -1,12 +1,23 @@
 package application;
 
+import entities.TrabalhadorComissao;
+import entities.TrabalhadorHora;
 import entities.TrabalhadorPeca;
 
 public class MainVencimentos {
 
     public static void main(String[] args) {
 
-        new TrabalhadorPeca("Jorge Silva", 53,62);
+        TrabalhadorPeca  tp = new TrabalhadorPeca("Jorge Silva", 53,62);
+        TrabalhadorComissao tc = new TrabalhadorComissao("Susana Ferreira", 650.00f, 2731.50f, 4.25f);
+        TrabalhadorHora th = new TrabalhadorHora("Carlos Miguel", 168, 4.50f);
+
+       Object [] trabs = new Object[10];
+
+       trabs[0] = tp;
+       trabs[1] = tc;
+       trabs[2] = th;
+       
 
     }
 }
